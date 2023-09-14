35124b7034812c587a41facf54d5e811b513ed14
hanan <hnan.akawi@gmail.com>
2023-09-14 15:00:04 +0300

    today design xml to do add image,fix errors

--- app/src/main/java/com/example/hananapp/MainActivity.java
+++ app/src/main/java/com/example/hananapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hananapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +12,37 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("HA","onCreate");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        //هو كائن مرئي يعرض رسالة نصصية على الشاشة لفترة قصيرة ثم تختفي تلقائيا
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("HA","onRestart");
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("HA","onStart");
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("HA","onResume");
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("HA","onPause");
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
     }
 }
